85d291a821131288f3a68a56ac5100187dbc50e7
Guillo C <56337629+0xGC@users.noreply.github.com>
2021-06-29 13:50:57 +0000

    Añadidos los threads y ver solo por pantalla
    Mejora de la velocidad, pero se pega alguna que otra ostia

--- lookuper.py
+++ lookuper.py
@@ -1,67 +1,126 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 # ====== LOOKUPER.PY [Ver. 0.2] ==========================================
 # Descubridor de hosts mediante consultas DNS dentro de una red
-# El concepto es bruteforce para DNS
+# El concepto es "bruteforce" para DNS
 # Guillo (github.com/0xGC)
 # Noviembre 2020
 #
 # dependencias externas:
 #     colorama
 #     yaspin
+#     dns
 # ========================================================================
 
 # Cosas que le faltan:
 # - poder decidir en que exportarlo (si csv o html o xml)
-# - Usar threads para paralelizar el trabajo y decrementar el tiempo
-# - Usar libreria DNS de python (para dejar de usar nslookup)
 
-import subprocess as sprc
+from dns import resolver, reversename, exception
 import csv
-import re
 from yaspin import yaspin
-from colorama import Fore
-from colorama import Style
 import ipaddress as ipa
 import argparse
-version = "Version 0.2"
-banner= f'''
-    {Fore.RED}██╗      ██████╗  ██████╗ ██╗  ██╗██╗   ██╗██████╗ ███████╗██████╗
+import re
+from prompt_toolkit import HTML, print_formatted_text
+from prompt_toolkit.styles import Style
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+
+def pinta (rollo, text1, text2=''):
+    if rollo == "good":
+        ico = "[+]"
+        style = Style.from_dict({
+        'icon': '#4caf50',
+        'msg': 'bold',
+        'sub-msg': '#cccccc italic'
+        })
+
+    elif rollo == "bad":
+        ico = "[-]"
+        style = Style.from_dict({
+        'icon': '#e0300a bold',
+        'msg': 'bold',
+        'sub-msg': '#cccccc italic'
+        })
+    
+    elif rollo == "warn":
+        ico = "[-]"
+        style = Style.from_dict({
+        'icon': '#ffc900',
+        'msg': 'bold',
+        'sub-msg': '#cccccc italic'
+        })
+
+    with sp.hidden():
+        print_formatted_text(HTML(
+            f'<b><icon>{ico}</icon></b> <msg>{text1}</msg> <sub-msg>{text2}</sub-msg>'
+        ), style=style)
+
+version = "0.5"
+
+def banner(): 
+    style = Style.from_dict({
+    'ban': '#167cc7',
+    'sub-ban': '#a6ffeb italic',
+    'text': '#4fabd6',
+    'version': '#8103f8 bold'
+    })
+    print_formatted_text(HTML(f'''<ban>
+    ██╗      ██████╗  ██████╗ ██╗  ██╗██╗   ██╗██████╗ ███████╗██████╗
     ██║     ██╔═══██╗██╔═══██╗██║ ██╔╝██║   ██║██╔══██╗██╔════╝██╔══██╗
-    ██║     ██║   ██║██║   ██║█████╔╝ ██║   ██║██████╔╝█████╗  ██████╔╝{Fore.RESET}
+    ██║     ██║   ██║██║   ██║█████╔╝ ██║   ██║██████╔╝█████╗  ██████╔╝</ban><sub-ban>
     ██║     ██║   ██║██║   ██║██╔═██╗ ██║   ██║██╔═══╝ ██╔══╝  ██╔══██╗
     ███████╗╚██████╔╝╚██████╔╝██║  ██╗╚██████╔╝██║     ███████╗██║  ██║
-    {Fore.RED}╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝{Fore.RESET}
-===| ### {Fore.RED}{Style.BRIGHT}DNS BRUTEFORCER {Fore.RESET}[{version}]{Style.RESET_ALL} ###  |=======[{Fore.GREEN}github.com/0xGC{Fore.RESET}]====>
-'''
+    ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝</sub-ban><text>
+    ===| ### DNS BRUTEFORCER [</text><version>{version}</version><text>] ###  |=======[</text><version>github.com/0xGC</version><text>]====></text>
+    '''), style=style)
 
 parser = argparse.ArgumentParser(description='DNS Bruteforcer de reconocimiento')
 parser.add_argument('ip_range',
                     help="Rango de IPs en notacion CIDR (ej:192.168.1.0/24)")
-parser.add_argument('--sep', default=',',
-                    help="formato del separador (por defecto: , ")                    
+parser.add_argument('--sep','-s', default=',',
+                    help="formato del separador (por defecto: , ")
+parser.add_argument('--screen','-scr', help="Solo output por pantalla", action='store_true')
+parser.add_argument('--workers','-w', type=int, help="Numero de threads concurrentes", default=10)                         
 
 args = parser.parse_args()
 pattern = re.compile(r'\bname = .*')
 
-sip = re.sub(r'\D','_', args.ip_range)
-outfilename = sip + "_output.csv"
-f = open(outfilename,'w')
-writer = csv.writer(f)
-print(banner)
-with yaspin(text="Bucando...", color="yellow") as sp:
-    for ip in ipa.IPv4Network(args.ip_range):    
-        sp.text=f"Rango en {ip}"
-        cmd = sprc.run(['nslookup',str(ip)],stdout=sprc.PIPE,)
-        out = cmd.stdout.decode('utf-8')
-        try:
-            name = pattern.search(out).group()
-            name = name[7:]
-            sp.write(f"Host {Fore.BLUE}{name}{Fore.RESET} encontrado en {Fore.GREEN}{ip}{Fore.RESET}")
+def consulta_dns(ip):
+    try:
+        addr=reversename.from_address(str(ip))
+        name = str(resolver.resolve(addr,"PTR")[0])
+        pinta("good",f"{name}({addr})",f"encontrado en {ip}")
+        if not args.screen:
             csv_out = [ip,name]
             writer.writerow(csv_out)
-        except:
-            pass
+    except exception.Timeout:
+        pinta("warn", "DNS Timeout")
+
+sip = re.sub(r'\D','_', args.ip_range)
+
+if not args.screen: 
+    outfilename = sip + "_output.csv"
+    f = open(outfilename,'w')
+    writer = csv.writer(f)
+
+banner()
+processes = []
+
+
+with yaspin(text=f"Bucando en {args.ip_range}...").blue.bold.shark.blue as sp:
+    with ThreadPoolExecutor(max_workers=args.workers) as executor:
+        for ip in ipa.IPv4Network(args.ip_range):    
+            # sp.text=f"Rango en {ip}"
+            processes.append(executor.submit(consulta_dns, ip))
+    
+    for task in as_completed(processes):
+        print(task.result())  
+    
     sp.text = "Escaneo de DNS completado!"
-    sp.ok("*")
-    sp.write(f'Archivos escritos en {Fore.GREEN}{Style.BRIGHT}{outfilename}{Style.RESET_ALL}{Fore.RESET}')
+    sp.green.ok("✔")
+    
+    if not args.screen:
+        pinta("good", f'Archivos escritos en {outfilename}' )
